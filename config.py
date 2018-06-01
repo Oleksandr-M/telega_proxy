476bbc21aa4e3a4d7ab0a90877b1fa66303228e8
unknown <alexander.makedonskiy@gmail.com>
2018-06-01 12:28:20 +0300

    OS deployment

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SECRET_KEY = 'you-will-never-guess'
 
 #DATABASE
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://rudeak:fkmnfdbcnf@localhost/telega'
+SQLALCHEMY_DATABASE_URI = 'mysql://rudeak:fkmnfdbcnf@mysql:3306/'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
